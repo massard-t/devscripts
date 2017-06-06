e83c60ae9ecec865836fd9f1711f610d0c2d0007
Theo Massard <massar_t@etna-alternance.net>
2017-06-06 14:13:04 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='devscripts',
+    package=['devscripts'],
+    version='0.0',
+    description='Scripts and functions to use while coding',
+    author='Theo Massard',
+    author_email='massar_t@etna-alternance.net',
+    url='https://github.com/massard-t/devscripts',
+    download_url='https://github.com/massard-t/devscripts/archive/0.0.tar.gz',
+    keywords=['scripts', 'functions', 'optimisation'],
+    classifiers=[],
+)
